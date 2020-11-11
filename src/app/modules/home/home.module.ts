a9b2b9b294dba1562a1abfad36b7c5e343f15870
Hiago Rios <hriosdev@gmail.com>
2020-11-11 09:55:14 -0300

    Alteraçao das variaveis de cores e criaçao do tema customizado do material

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
